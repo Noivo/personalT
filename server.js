00b71b45a4dfae52f6fa47c55fab39262925fbe4
jmnoivo <jmnoivo@gmail.com>
2020-08-04 15:30:15 +0100

    Path to client

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ const app = express()
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("/client/build"))
+  app.use(express.static("client/build"))
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  // })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
 }
 
 const port = process.env.PORT || 5000

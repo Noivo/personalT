2ade4e3c35b24bf0e5ba5897104ddb6109f132dd
jmnoivo <jmnoivo@gmail.com>
2020-08-06 16:51:46 +0100

    Changed routes order

--- routes/api/clients.js
+++ routes/api/clients.js
@@ -1,12 +1,6 @@
 const router = require("express").Router()
 let Client = require("../../models/client.model")
 
-router.route("/").get((req, res) => {
-  Client.find()
-    .then((clients) => res.json(clients))
-    .catch((err) => res.status(400).json("Error: " + err))
-})
-
 router.route("/add").post((req, res) => {
   const newClient = new Client({
     name: req.body.name,
@@ -16,6 +10,12 @@ router.route("/add").post((req, res) => {
   newClient
     .save()
     .then(() => res.json("Client added!"))
+    .catch((err) => res.status(400).json("Error: " + err))
+})
+
+router.route("/").get((req, res) => {
+  Client.find()
+    .then((clients) => res.json(clients))
     .catch((err) => res.status(400).json("Error: " + err))
 })
 
--- routes/api/exercises.js
+++ routes/api/exercises.js
@@ -1,12 +1,6 @@
 const router = require("express").Router()
 let Exercise = require("../../models/exercise.model")
 
-router.route("/").get((req, res) => {
-  Exercise.find()
-    .then((exercises) => res.json(exercises))
-    .catch((err) => res.status(400).json("Error: " + err))
-})
-
 router.route("/add").post((req, res) => {
   const newExercise = new Exercise({
     username: req.body.username,
@@ -20,6 +14,12 @@ router.route("/add").post((req, res) => {
   newExercise
     .save()
     .then(() => res.json("Exercise added!"))
+    .catch((err) => res.status(400).json("Error: " + err))
+})
+
+router.route("/").get((req, res) => {
+  Exercise.find()
+    .then((exercises) => res.json(exercises))
     .catch((err) => res.status(400).json("Error: " + err))
 })
 

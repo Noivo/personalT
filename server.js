0dc08d3924f2c8ba1fbaaf04c06008fd112bfd21
jmnoivo <jmnoivo@gmail.com>
2020-08-07 19:06:55 +0100

    Refactor server

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const app = express()
 const port = process.env.PORT || 5000
 app.use(cors())
 
-// Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
     extended: false,
@@ -23,26 +22,18 @@ app.use(
 )
 app.use(bodyParser.json())
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"))
+app.use("/api/users", users)
+app.use("/api/exercises", exercises)
+app.use("/api/clients", clients)
 
-  app.use("/api/users", users)
-  app.use("/api/exercises", exercises)
-  app.use("/api/clients", clients)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   })
 }
 
-// Routes
-// app.use("/api/users", users)
-// app.use("/api/exercises", exercises)
-// app.use("/api/clients", clients)
-
-// DB config
 const db = require("./config/keys").mongoURI
 const MONGODB_URI = require("./config/keys").MONGODB_URI
 mongoose.connect(MONGODB_URI || db, {
@@ -54,9 +45,7 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully")
 })
 
-// Passport middleware
 app.use(passport.initialize())
-// Passport config
 require("./config/passport")(passport)
 
 app.listen(port, () => {
